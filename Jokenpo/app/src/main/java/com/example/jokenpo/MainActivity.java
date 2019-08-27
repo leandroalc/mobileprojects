157c0300b421f9adf66fd4df15467b229286cefd
Aluno <aluno@expert.local>
2019-08-27 10:22:42 -0300

    Início do projeto Jokenpo

---
+++ Jokenpo/app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.jokenpo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(){
+
+    }
+
+    public void selecionarPapel(){
+
+    }
+
+
+    public void selecionarTesoura(){
+
+    }
+
+    public void selecionarOpcao(){
+
+    }
+
+}
